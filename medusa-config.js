0de7fabcf0130f532e2118a37c8e4393527f484d
vishal-gits <123520641+vishal-gits@users.noreply.github.com>
2024-06-09 05:36:55 +0530

    updating config.js

--- medusa-config.js
+++ medusa-config.js
@@ -90,6 +90,7 @@ const plugins = [
       api_key: process.env.STRIPE_API_KEY,
       capture: true,
       automatic_payment_methods: true,
+      payment_description: "This is for international payments",
       // webhook_secret: process.env.STRIPE_WEBHOOK_SECRET,
     },
   },
