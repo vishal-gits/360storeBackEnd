87392f6f8980d77e5a9f4447e8240fb3142861d3
vishal-gits <123520641+vishal-gits@users.noreply.github.com>
2024-06-11 12:59:45 +0530

    commented for admin plugin in config and removed build from package.json from build command

--- medusa-config.js
+++ medusa-config.js
@@ -42,17 +42,17 @@ const plugins = [
       upload_dir: "uploads",
     },
   },
-  {
-    resolve: "@medusajs/admin",
-    /** @type {import('@medusajs/admin').PluginOptions} */
-    options: {
-      autoRebuild: true,
-      develop: {
-        open: process.env.OPEN_BROWSER !== "false",
-      },
-      enableUI: true,
-    },
-  },
+  // {
+  //   resolve: "@medusajs/admin",
+  //   /** @type {import('@medusajs/admin').PluginOptions} */
+  //   options: {
+  //     autoRebuild: true,
+  //     develop: {
+  //       open: process.env.OPEN_BROWSER !== "false",
+  //     },
+  //     enableUI: true,
+  //   },
+  // },
   {
     resolve: `medusa-plugin-algolia`,
     options: {
